c6663f6c838751082a4b11a5328dee6f03614b10
avidLearnerInProgress <chiragshah9696@gmail.com>
2019-03-21 11:49:14 +0530

    Added files

--- mnemonics_of_phone_number.cpp
+++ mnemonics_of_phone_number.cpp
@@ -20,6 +20,25 @@ void recurviseSolve(string digits, string cycle, vector<string>& combinations, i
 }
 
 
+/* Iterative approach:
+vector<string> letterCombinations(string digits) {
+    vector<string> res;
+    string charmap[10] = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    res.push_back("");
+    for (int i = 0; i < digits.size(); i++)
+    {
+        vector<string> tempres;
+        string chars = charmap[digits[i] - '0'];
+        for (int c = 0; c < chars.size();c++)
+            for (int j = 0; j < res.size();j++)
+                tempres.push_back(res[j]+chars[c]);
+        res = tempres;
+    }
+    return res;
+}
+*/
+
+
 int main() {
 	
 	//int y; cin>>y;
