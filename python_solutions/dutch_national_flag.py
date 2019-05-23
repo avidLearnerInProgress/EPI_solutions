cef345eda31a01712d6c87c0e079dd963f3a8fa7
avidLearnerInProgress <chiragshah9696@gmail.com>
2019-05-24 00:30:15 +0530

    Added Pythonic implementation

--- python_solutions/dutch_national_flag.py
+++ python_solutions/dutch_national_flag.py
@@ -1,41 +1,19 @@
 RED, WHITE, BLUE = range(3)
 
-def dutch_flag_partition(pivot_ind, A):
-    '''pivot = A[pivot_ind]
-    smaller = 0
-    for i in range(len(A)):
-        if A[i] < pivot:
-            A[i], A[smaller] = A[smaller], A[i]
-            smaller += 1
-    larger = len(A) - 1
-    for i in reversed(range(len(A))):
-        if A[i] < pivot:
-            break
-        elif A[i] > pivot:
-            A[i], A[larger] = A[larger], A[i] 
-            larger -= 1      
-    return A'''
+def dutch_flag_partition(A):
 
-    small, current, large = 0, 0, len(A) - 1
+    smallest, current, largest = 0, 0, len(A) - 1
+    while current <= largest:
+        if A[current] == RED:
+            A[smallest], A[current] = A[current], A[smallest]
+            smallest += 1
+            current += 1
+        elif A[current] == WHITE:
+            current += 1
+        else:
+            A[largest], A[current] = A[current], A[largest]
+            largest -= 1
+    return A
 
+print(dutch_flag_partition([0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]))
 
-print(dutch_flag_partition(6, [10, 40, 80, 70, 90, 12, 13]))
-
-
-'''
-  int smallest = 0, current = 0, largest = n - 1;
-            
-            while(current < largest){
-                if(A[current] < A[pivot]){
-                    swap(A[smallest], A[current]);
-                    smallest++; current ++;
-                }
-                else if(A[current] == A[pivot])
-                    current++;
-                else{
-                    swap(A[current], A[largest]);
-                    current++; largest--;
-                }
-            }
-          
-'''
