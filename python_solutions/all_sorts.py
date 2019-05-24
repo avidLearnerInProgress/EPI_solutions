79b749ebb3bd56a4e21485e9290dc24003a0cbc0
avidLearnerInProgress <chiragshah9696@gmail.com>
2019-05-24 15:20:53 +0530

    Updated solution

--- python_solutions/all_sorts.py
+++ python_solutions/all_sorts.py
@@ -13,6 +13,37 @@ def insertionSort(A):
 def callInsertionSort():
     print(insertionSort([4, 3, 2, 9, 10]))
 
+#############-----------------------#############
+def binarySearch(arr, value, low, high):
+    if high <= low:
+        return (low + 1) if value > arr[low] else low;
+    mid = int((low + high)/2)
+
+    if value == arr[mid]:
+        return mid + 1
+    elif value > arr[mid]:
+        return binarySearch(arr, value, mid + 1, high)
+    else:
+        return binarySearch(arr, value, low, mid - 1)    
+
+def binaryInsertionSort(arr):
+
+    for i in range(1, len(arr)):
+        value = arr[i]
+        j = i - 1
+        loc = binarySearch(arr, value, 0, j)
+
+        while j >= loc:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = value
+    return arr
+
+def callBinaryInsertionSort():
+    print(binaryInsertionSort([4, 3, 2, 9, 10]))
+
+#############-----------------------#############
+
 #https://ide.geeksforgeeks.org/ub0kWiG01z
 def lomutoPartition(A, low, high):
     pivot = A[high]
@@ -36,9 +67,8 @@ def callQuickSort():
     low, high = 0, len(A) - 1
     print(quickSort(A, low, high))
 
-def callMergeSort():
-    pass
 
 if __name__ == "__main__":
     callInsertionSort()
     callQuickSort()
+    callBinaryInsertionSort()
